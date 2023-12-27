183b6fb9afe84e31b7442cf35afc7232e3a8d743
hcorredor <hcorredor@novopayment.com>
2023-12-27 15:28:56 -0500

    ajusta

--- src/app/api/conectApi/route.ts
+++ src/app/api/conectApi/route.ts
@@ -6,6 +6,6 @@ export async function POST(request: NextRequest) {
     return Response.json(json);
   } catch (e) {
     console.log(e, 'no leyo nada del body');
-    return new Response(null, { status: 200 });
+    return new Response('ok', { status: 200 });
   }
 }
