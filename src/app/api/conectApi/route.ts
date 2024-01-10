31642ffeaead3373161048b9fc6026a6b0190ad9
hcorredor <hcorredor@novopayment.com>
2024-01-09 20:23:25 -0500

    ajusta

--- src/app/api/conectApi/route.ts
+++ src/app/api/conectApi/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
+  console.log('🚀 ~ file: route.ts:4 ~ POST ~ req:', req.headers);
+  console.log('🚀 ~ file: route.ts:4 ~ POST ~ req:', req.formData);
   console.log('🚀 ~ file: route.ts:4 ~ POST ~ req:', req.body);
   console.log('🚀 ~ file: route.ts:4 ~ POST ~ req:', req.method);
   const body = await req.json();
